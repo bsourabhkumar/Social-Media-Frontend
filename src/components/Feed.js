5de5e10fdbb28e4d5a11b0594d212f676c97daee
Sourabh Kumar <bsourabhkumar@gmail.com>
2021-10-15 00:37:01 +0530

    Added Backend API for getting posts

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -16,7 +16,7 @@ const Feed = ({username}) => {
             // const res = username 
             // ? await axios.get("posts/profile/" + username) 
             // : await axios.get("posts/timeline/" + user._id )
-            const res = await axios.get("posts/timeline/" + user._id );
+            const res = await axios.get("https://aqueous-reef-25837.herokuapp.com/api/posts/timeline/" + user._id );
             const newPost = res.data.sort((p1, p2) => {
                 return new Date(p2.createdAt) - new Date(p1.createdAt);
             })
